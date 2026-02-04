6247855285687ba7f4d01ffd84a70e8821aa1bbf
RDOLopes <doliveiralopes@yahoo.com.br>
2026-02-04 20:43:35 -0300

    aula 95: fazendo dispach de CHANGE_SETTINGS para salvar as configurações no estado.

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -6,22 +6,77 @@ import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../templates/MainTemplate';
 import React, { useRef } from 'react';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { showMessage } from '../../adapters/showMessage';
+import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
 
 export function Settings() {
-  const { state } = useTaskContext();
+  const { state, dispatch } = useTaskContext();
   const workTimeInput = useRef<HTMLInputElement>(null);
   const shortBreakTimeInput = useRef<HTMLInputElement>(null);
   const longBreakTimeInput = useRef<HTMLInputElement>(null);
 
   function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    showMessage.dismiss();
 
-    const workTime = workTimeInput.current?.value;
-    const shortBreakTime = shortBreakTimeInput.current?.value;
-    const longBreakTime = longBreakTimeInput.current?.value;
+    const formErrors = [];
+    const workTime = Number(workTimeInput.current?.value);
+    const shortBreakTime = Number(shortBreakTimeInput.current?.value);
+    const longBreakTime = Number(longBreakTimeInput.current?.value);
 
-    console.log(workTime, shortBreakTime, longBreakTime);
-    // console.log('ENVIADO', Date.now());
+    if (isNaN(workTime)) {
+      formErrors.push('Por favor use apenas números para foco!');
+    }
+
+    if (isNaN(shortBreakTime)) {
+      formErrors.push('Apenas números para descanso curto');
+    }
+
+    if (isNaN(longBreakTime)) {
+      formErrors.push('Apenas números para descanso longo');
+    }
+
+    if (workTime < 1 || workTime > 99) {
+      formErrors.push('Digite valores entre 1 e 99 para foco meu consagrado!');
+    }
+
+    if (shortBreakTime < 1 || shortBreakTime > 30) {
+      formErrors.push(
+        'Digite valores entre 1 e 30 para descanso curto meu consagrado!',
+      );
+    }
+
+    if (longBreakTime < 1 || longBreakTime > 60) {
+      formErrors.push(
+        'Digite valores entre 1 e 60 para descanso longo meu consagrado!',
+      );
+    }
+
+    if (
+      !Number.isInteger(workTime) ||
+      !Number.isInteger(longBreakTime) ||
+      !Number.isInteger(shortBreakTime)
+    ) {
+      formErrors.push('Digite valores inteiros meu consagrado!');
+    }
+
+    if (formErrors.length > 0) {
+      formErrors.forEach(error => {
+        showMessage.error(error);
+      });
+      return;
+    }
+
+    dispatch({
+      type: TaskActionTypes.CHANGE_SETTINGS,
+      payload: {
+        workTime,
+        shortBreakTime,
+        longBreakTime,
+      },
+    });
+
+    showMessage.success('Deu bom meu consagrado.');
   }
 
   return (
@@ -43,15 +98,16 @@ export function Settings() {
               labelText='Foco'
               ref={workTimeInput}
               defaultValue={state.config.workTime}
+              type='number'
             />
           </div>
-
           <div className='formRow'>
             <DefaultInput
               id='shortBreakTime'
               labelText='Descanso curto'
               ref={shortBreakTimeInput}
               defaultValue={state.config.shortBreakTime}
+              type='number'
             />
           </div>
           <div className='formRow'>
@@ -60,6 +116,7 @@ export function Settings() {
               labelText='Descanso longo'
               ref={longBreakTimeInput}
               defaultValue={state.config.longBreakTime}
+              type='number'
             />
           </div>
           <div className='formRow'>
